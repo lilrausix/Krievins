7023b4268163f5370e5cf0fd478c21fd8f82e807
Roberts <ipa24.r.eglitis@vtdt.edu.lv>
2025-12-02 19:52:59 +0200

    s

---
+++ ienakt.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ienākt</title>
+    <link rel="stylesheet" href="css/ienakt.css">
+    <link rel="icon" type="image/x-icon" href="bildes/logo.png">
+
+</head>
+<body>
+    <div class="nav">
+        <div class="logo">
+        <img src="bildes/logo.png" alt="PB logo" style="width:50px; height:auto;">
+        </div>
+        <div class="pogas">
+        <a href="index.php">
+            <button type="button">Sākums</button>
+        </a>
+        <a href="parmums.php">
+            <button type="button">Par mums</button>
+        </a>
+        <a href="kontakti.php">
+        <button type="button">Kontakti</button>
+        </a>
+        <a href="veikals.php">
+            <button type="button">Veikals</button>
+        </a>
+        <a href="reg.php">
+            <button type="button">Reģistrēšanās</button>
+        </a>
+        </div>
+
+    </div>
+
+    <div class="wrapper">
+    <h2>Ienākt</h2>
+    <form action="#">
+      <div class="input-box">
+        <input type="text" placeholder="lietotājvārds" required>
+      </div>
+
+      <div class="input-box">
+        <input type="password" placeholder="parole" required>
+      </div>
+      <div class="input-box button">
+        <input type="Submit" value="Ienākt">
+      </div>
+      
+    </form>
+  </div>
+    
+</body>
+</html>
---
+++ reg.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reģistrēšanās</title>
+    <link rel="stylesheet" href="css/reg.css">
+    <link rel="icon" type="image/x-icon" href="bildes/logo.png">
+
+</head>
+<body>
+    <div class="nav">
+        <div class="logo">
+        <img src="bildes/logo.png" alt="PB logo" style="width:50px; height:auto;">
+        </div>
+        <div class="pogas">
+        <a href="sakums.php">
+            <button type="button">Sākums</button>
+        </a>
+        <a href="parmums.php">
+            <button type="button">Par mums</button>
+        </a>
+        <a href="kontakti.php">
+        <button type="button">Kontakti</button>
+        </a>
+        <a href="veikals.php">
+            <button type="button">Veikals</button>
+        </a>
+        <a href="reg.php">
+            <button type="button">Reģistrēšanās</button>
+        </a>
+        </div>
+
+    </div>
+
+    <div class="wrapper">
+    <h2>Reģistrēšanās</h2>
+    <form action="#">
+      <div class="input-box">
+        <input type="text" placeholder="Ievadi savu lietotājvārdu" required>
+      </div>
+      <div class="input-box">
+        <input type="text" placeholder="Ievadi savu epastu" required>
+      </div>
+      <div class="input-box">
+        <input type="password" placeholder="Izveido paroli" required>
+      </div>
+      <div class="input-box">
+        <input type="password" placeholder="Apstiprini  paroli" required>
+      </div>
+      <div class="policy">
+        <input type="checkbox">
+        <h3>Es piekrītu visiem noteikumiem un nosacījumiem</h3>
+      </div>
+      <div class="input-box button">
+        <input type="Submit" value="Reģistrēties tagad">
+      </div>
+      <div class="text">
+        <h3>Tev jau ir konts? <a href="ienakt.php">Ieiet</a></h3>
+      </div>
+    </form>
+  </div>
+    
+</body>
+</html>
+
